82732d927f1bb421c490f924fc5b9bb80146c0d6
sapna07raj <raj23sapna@gmail.com>
2026-08-05 08:03:22 +0530

    Delete Devops_jenkins/gradle_final directory

--- Devops_jenkins/gradle_final/src/com/tyit/HelloJenkins.java
+++
@@ -1,8 +0,0 @@
-package com.tyit;
-
-public class HelloJenkins {
-
-	public static void main(String[] args) {
-        System.out.println("Hello Jenkins! Java project built successfully.");
-    }
-}
